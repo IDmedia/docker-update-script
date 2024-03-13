eabccd394223429640a0cde19e0615ef124a6255
Benjamin Krause <neoid@animenord.com>
2024-03-13 21:29:25 +0100

    May be better to just check for build command so the Dockerfile can be in a subfolder

--- update.py
+++ update.py
@@ -2,6 +2,7 @@ import os
 import re
 import sys
 import json
+import logging
 import argparse
 import subprocess
 from collections import Counter
@@ -11,6 +12,52 @@ def get_image_ids(compose_file):
     # Get the image IDs from the specified docker-compose file
     output = subprocess.check_output(['docker-compose', '-f', compose_file, 'images', '-q']).decode().strip()
     return output.splitlines()
+
+
+def get_docker_container_state(container_id):
+    try:
+        # Run the docker inspect command and capture the output
+        output = subprocess.check_output(['docker', 'inspect', container_id], universal_newlines=True)
+
+        # Parse the JSON output
+        container_info = json.loads(output)
+
+        # Extract the state from the output
+        if container_info:
+            # The state is available under the 'State' key
+            state = container_info[0].get('State', {})
+            if state:
+                return state
+
+        # No container found with the given ID or no state available
+        return None
+
+    except subprocess.CalledProcessError:
+        logger.error(f"Failed to retrieve container state from container id '{container_id}'")
+        return None
+
+
+def get_docker_container_state_from_compose(yaml_path):
+    try:
+        # Run the docker-compose ps -q command to get container IDs
+        output = subprocess.check_output(['docker-compose', '-f', yaml_path, 'ps', '-q'], universal_newlines=True)
+
+        # Split the output into lines and remove empty lines
+        container_ids = [line.strip() for line in output.splitlines() if line.strip()]
+
+        container_states = []
+
+        # Loop through container IDs and get their states
+        for container_id in container_ids:
+            container_state = get_docker_container_state(container_id)
+            if container_state:
+                container_states.append(container_state)
+
+        return container_states
+
+    except subprocess.CalledProcessError:
+        logger.error(f"Failed to retrieve container state from docker-compose file '{yaml_path}'")
+        return None
 
 
 def get_docker_tag(image_sha):
@@ -32,7 +79,7 @@ def get_docker_tag(image_sha):
         return None
 
     except subprocess.CalledProcessError:
-        # Handle the case where the docker command fails
+        logger.error(f"Failed to retrieve container tag from image SHA '{image_sha}'")
         return None
 
 
@@ -65,8 +112,8 @@ def authenticate_docker_registries():
                                    shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
                     docker_registries.append(registry)
                 except subprocess.CalledProcessError as e:
-                    print(f'Docker login failed with exit code {e.returncode}')
-                    print('An error occurred during the login process. Please check your credentials.')
+                    logger.error(f'Docker login failed with exit code {e.returncode}')
+                    logger.error('An error occurred during the login process. Please check your credentials.')
                     sys.exit(1)
 
     return docker_registries
@@ -96,19 +143,22 @@ def main(args):
 
     for container in containers:
         compose_file = os.path.join(docker_dir, container, 'docker-compose.yaml')
-        docker_file = os.path.join(docker_dir, container, 'Dockerfile')
 
         # Process each valid docker-compose file
         if not os.path.isfile(compose_file):
+            logger.warning(f"Skipping container '{container}' because docker-compose.yaml is missing")
             continue
 
         # Get current image id
+        logger.info(f"Updating '{container}'")
         current_image_ids = get_image_ids(compose_file)
 
         # Build or pull the latest image
-        if os.path.isfile(docker_file) and build_in_docker_compose(compose_file):
+        if build_in_docker_compose(compose_file):
+            logger.info(f"Initiating build of '{container}' specified by 'build' attribute in docker-compose.yaml")
             subprocess.check_call(['docker-compose', '-f', compose_file, 'build', '--no-cache'])
         else:
+            logger.info(f"Pulling new image for '{container}'")
             subprocess.check_call(['docker-compose', '-f', compose_file, 'pull'])
 
         # Check the new tag the container has
@@ -117,7 +167,13 @@ def main(args):
 
         # Check if the image IDs have changed or force re-creation
         if (Counter(current_image_ids) != Counter(new_image_ids)) or args.force:
+            if args.force:
+                logger.warning(f"Adding '{container}' to restart list (forced)")
+            else:
+                logger.warning(f"Adding '{container}' to restart list as a newer version is available")
             containers_restart.append(compose_file)
+        else:
+            logger.info(f"Image for '{container}' remains unchanged, no container restart required")
 
     # Restart selected containers
     for compose_file in containers_restart:
@@ -130,9 +186,11 @@ def main(args):
 
     # Docker logout from authenticated registries
     for registry in docker_registries:
+        logger.info(f"Logging out from Docker registry: {registry}")
         subprocess.run(f'docker logout {registry}', shell=True, check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     # Docker prune unused resources
+    logger.info('Pruning unused Docker resources')
     subprocess.check_call(['docker', 'image', 'prune', '-f'])
     subprocess.check_call(['docker', 'volume', 'prune', '-f'])
     subprocess.check_call(['docker', 'builder', 'prune', '-f'])
@@ -140,6 +198,33 @@ def main(args):
 
 
 if __name__ == '__main__':
+    # Define color codes
+    COLORS = {
+        'INFO': '\033[0m',     # White
+        'WARNING': '\033[93m',  # Yellow
+        'ERROR': '\033[91m',    # Red
+        'CRITICAL': '\033[91m'  # Red
+    }
+    RESET_COLOR = '\033[0m'  # Reset color to default
+
+    # Custom formatter
+    class ColoredFormatter(logging.Formatter):
+        def format(self, record):
+            levelname = record.levelname
+            msg = super().format(record)
+            color = COLORS.get(levelname, '')
+            return f"{color}{msg}{RESET_COLOR}"
+
+    # Remove basicConfig() and directly configure the root logger
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+
+    # Add colored formatter to the default stream handler
+    handler = logging.StreamHandler()
+    formatter = ColoredFormatter('%(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
     # Command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-c', '--containers', type=str, help='Specify a list of containers to update (example: "couchpotato, medusa")', required=False)
